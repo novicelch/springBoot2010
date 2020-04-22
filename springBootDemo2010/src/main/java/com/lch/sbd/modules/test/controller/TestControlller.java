dfdcbefed7f3d28f06c0b41d35a5af14b4d8bcc1
liuchenhu <liuchenhu@DESKTOP-CN400SO>
2020-04-22 18:46:13 +0800

    "2020-4-22:properties, log, application-{profile}, devtools,http, https"

--- springBootDemo2010/src/main/java/com/lch/sbd/modules/test/controller/TestControlller.java
+++ springBootDemo2010/src/main/java/com/lch/sbd/modules/test/controller/TestControlller.java
@@ -1,12 +1,73 @@
 package com.lch.sbd.modules.test.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.lch.sbd.modules.test.vo.ConfigBean;
+
 @Controller
 public class TestControlller {
 	
+	private final static Logger LOGGER = LoggerFactory.getLogger(TestControlller.class);
+	
+	@Value("${server.port}")
+	private int port;
+	@Value("${com.lch.name}")
+	private String name;
+	@Value("${com.lch.age}")
+	private int age;
+	@Value("${com.lch.desc}")
+	private String desc;
+	@Value("${com.lch.random}")
+	private String random;
+	
+	@Autowired
+	private  ConfigBean configBean;
+	
+	/**
+	 * http://127.0.0.1:8080/test/log
+	 *
+	 */
+		
+	@RequestMapping("test/log")
+	@ResponseBody
+	public String logTest(){
+		LOGGER.trace("This is TRACE log.");
+		LOGGER.debug("This is DEBUG log.");
+		LOGGER.info("This is INFO log.");
+		LOGGER.warn("This is WARN log.");
+		LOGGER.error("This is ERROR log.222222");
+		return "this is log test";
+		
+	}
+	/**
+	 * http://127.0.0.1/test/config
+	 */
+	@RequestMapping("/test/config")
+	@ResponseBody
+	public String configTest(){
+		StringBuffer sb = new StringBuffer();
+		sb.append(port).append("-----")
+		.append(name).append("----")
+		.append(age).append("----")
+		.append(desc).append("----")
+		.append(random).append("----").append("<br/>");
+		sb.append(configBean.getName()).append("------")
+		.append(configBean.getAge()).append("------")
+		.append(configBean.getDesc()).append("------")
+		.append(configBean.getRandom()).append("------");
+		return sb.toString();
+	}
+	
+	/**
+	 * 
+	 * http://127.0.0.1/test/appDesc
+	 */
 	@RequestMapping("/test/appDesc")
 	@ResponseBody
 	public String getDsc(){
